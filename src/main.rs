e8774254af6772e6cffa6f6059763eaad33455a4
bczhc <bczhc0@126.com>
2021-04-10 13:36:24 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
-    f();
+    let a: i32 = 2;
+
+    if a == 2 {
+        for _i in 0..100 {
+            f();
+        }
+    }
 }
 
 fn f() {
-    print!("hello, world");
+    let s: &str = "hello, world";
+    println!("{}, {}", s, s);
 }
