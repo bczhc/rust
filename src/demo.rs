bba1638a2013b0e8bd38a936496afc126ea5de61
bczhc <bczhc0@126.com>
2021-05-15 23:09:42 +0800

    GTK GUI demo

--- src/demo.rs
+++ src/demo.rs
@@ -1,1 +1,25 @@
-fn main() {}
+extern crate gtk;
+use gtk::prelude::*;
+use gtk::{ButtonsType, DialogFlags, MessageDialog, MessageType, ResponseType, Window};
+
+fn main() {
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+    let response_type = MessageDialog::new(
+        None::<&Window>,
+        DialogFlags::empty(),
+        MessageType::Info,
+        ButtonsType::Ok,
+        "Hallo du Lama!",
+    )
+    .run();
+
+    match response_type {
+        ResponseType::Ok => {
+            println!("hh");
+        }
+        _ => {}
+    }
+}
