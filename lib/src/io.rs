bb13d30896965362cd658a6edc23402f20d04329
bczhc <bczhc0@126.com>
2021-05-16 06:46:27 +0800

    draw points

--- lib/src/io.rs
+++ lib/src/io.rs
@@ -1,4 +1,6 @@
+use std::fs::{File, OpenOptions};
 use std::io::{ErrorKind, Read};
+use std::path::Path;
 
 pub trait ReadLine {
     fn read_line(&mut self) -> Option<String>;
@@ -25,3 +27,16 @@ impl ReadLine for dyn Read {
         return Some(String::from_utf8(read).unwrap());
     }
 }
+
+pub trait OpenOrCreate {
+    fn open_or_create<P: AsRef<Path>>(path: P) -> std::io::Result<File> {
+        OpenOptions::new()
+            .truncate(true)
+            .create(true)
+            .write(true)
+            .read(true)
+            .open(path.as_ref())
+    }
+}
+
+impl OpenOrCreate for File {}
--- src/demo.rs
+++ src/demo.rs
@@ -1,26 +1,39 @@
 use gio::prelude::ApplicationExtManual;
-use gio::ApplicationExt;
+use gio::{ApplicationExt, FileOutputStream, OutputStreamWrite};
 use glib::signal::Inhibit;
 use gtk::prelude::{BuildableExtManual, ObjectExt};
 use gtk::{ContainerExt, GtkWindowExt, TextViewExt, ToggleButtonExt, WidgetExt};
+use lib::io::OpenOrCreate;
+use std::cell::RefCell;
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::rc::Rc;
+
+static mut P: *mut BufWriter<File> = 0 as *const BufWriter<File> as *mut BufWriter<File>;
 
 fn main() {
+    let mut bw = BufWriter::new(File::open_or_create("./data").unwrap());
+    unsafe {
+        P = &bw as *const BufWriter<File> as *mut BufWriter<File>;
+    }
     let app = gtk::Application::new(Some("pers.zhc.demo.a"), gio::ApplicationFlags::FLAGS_NONE)
         .expect("error");
     app.connect_activate(|app| {
         let window = gtk::ApplicationWindow::new(app);
-        let tv = gtk::TextView::new();
-        tv.set_editable(true);
-        window.add(&tv);
-        window.set_title("hello");
-        tv.connect_event(|s, e| {
-            println!("{:?}", e);
-            Inhibit::default()
-        });
-        window.connect_event(|s, e| {
-            if e.get_event_type().to_string() == "EventType::MotionNotify" {
-                let i = e.get_coords().unwrap();
-                s.set_title(format!("{:?}", i).as_str());
+        window.connect_event(|s, e| unsafe {
+            let event_type = e.get_event_type();
+            let bw = P.as_mut().unwrap();
+            match event_type.to_string().as_str() {
+                "EventType::MotionNotify" => {
+                    let i = e.get_coords().unwrap();
+                    s.set_title(format!("{:?}", i).as_str());
+                    bw.write(format!("{} {}", i.0, i.1).as_bytes());
+                    bw.write(&[b'\n']);
+                }
+                "EventType::Destroy" => {
+                    bw.flush();
+                }
+                _ => {}
             }
             Inhibit::default()
         });
