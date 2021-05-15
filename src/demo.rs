34e7977e1a29a1ea391e4d7b03d5a8047f4f5be9
bczhc <bczhc0@126.com>
2021-05-16 00:16:02 +0800

    demo2

--- src/demo.rs
+++ src/demo.rs
@@ -1,25 +1,30 @@
-extern crate gtk;
-use gtk::prelude::*;
-use gtk::{ButtonsType, DialogFlags, MessageDialog, MessageType, ResponseType, Window};
+use gio::prelude::ApplicationExtManual;
+use gio::ApplicationExt;
+use glib::signal::Inhibit;
+use gtk::prelude::{BuildableExtManual, ObjectExt};
+use gtk::{ContainerExt, GtkWindowExt, TextViewExt, ToggleButtonExt, WidgetExt};
 
 fn main() {
-    if gtk::init().is_err() {
-        println!("Failed to initialize GTK.");
-        return;
-    }
-    let response_type = MessageDialog::new(
-        None::<&Window>,
-        DialogFlags::empty(),
-        MessageType::Info,
-        ButtonsType::Ok,
-        "Hallo du Lama!",
-    )
-    .run();
-
-    match response_type {
-        ResponseType::Ok => {
-            println!("hh");
-        }
-        _ => {}
-    }
+    let app = gtk::Application::new(Some("pers.zhc.demo.a"), gio::ApplicationFlags::FLAGS_NONE)
+        .expect("error");
+    app.connect_activate(|app| {
+        let window = gtk::ApplicationWindow::new(app);
+        let tv = gtk::TextView::new();
+        tv.set_editable(true);
+        window.add(&tv);
+        window.set_title("hello");
+        tv.connect_event(|s, e| {
+            println!("{:?}", e);
+            Inhibit::default()
+        });
+        window.connect_event(|s, e| {
+            if e.get_event_type().to_string() == "EventType::MotionNotify" {
+                let i = e.get_coords().unwrap();
+                s.set_title(format!("{:?}", i).as_str());
+            }
+            Inhibit::default()
+        });
+        window.show_all();
+    });
+    app.run(&[]);
 }
