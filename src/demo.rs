15cf1859e1fb2a6c321cc75cb918b1e20283b99f
bczhc <bczhc0@126.com>
2021-06-04 11:58:27 +0800

    compiled on Windows

--- src/demo.rs
+++ src/demo.rs
@@ -1,11 +1,4 @@
-use lib::libc::ToCString;
-
 fn main() -> Result<(), ()> {
-    let file = "/home/zhc/tmp/a";
-    let c_string = file.to_c_string();
-    let file_c_str = c_string.as_ptr();
-    let i = unsafe { libc::chmod(file_c_str, 755) };
-    println!("{}, {:?}", i, errno::errno());
-
+    println!("hello, world");
     Ok(())
 }
