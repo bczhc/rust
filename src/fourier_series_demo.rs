c441523c77d41b98bbef4159bc18b423f82c3d32
bczhc <bczhc0@126.com>
2021-08-11 04:53:49 +0800

    FourierSeries: use `Mutex` to ensure the concurrent safety

--- src/fourier_series_demo.rs
+++ src/fourier_series_demo.rs
@@ -3,11 +3,13 @@ use bczhc_lib::epicycle::Epicycle;
 use bczhc_lib::fourier_series::fourier_series_calc;
 use std::f64::consts::PI;
 use std::thread::spawn;
+use std::sync::Mutex;
 
 fn main() {
     let mut vec = Vec::new();
+    let vec_mutex = Mutex::new(vec);
 
-    let p = &vec as *const Vec<Epicycle> as usize;
+    let p = &vec_mutex as *const Mutex<Vec<Epicycle>> as usize;
     fourier_series_calc(
         100,
         PI * 2.0,
@@ -15,9 +17,11 @@ fn main() {
         10000,
         |t| ComplexValueF64::new(1.0, 2.0 + t),
         move |r| unsafe {
-            (&mut *(p as *mut Vec<Epicycle>)).push(r);
+            let mut guard = (&mut *(p as *mut Mutex<Vec<Epicycle>>)).lock().unwrap();
+            guard.push(r);
         },
     );
 
-    println!("{:?}", vec);
+    let guard = vec_mutex.lock().unwrap();
+    println!("{:?}", &*guard);
 }
