5dc17229b7651a5d4871898d635f9567b76500fb
bczhc <bczhc0@126.com>
2021-08-24 23:42:00 +0800

    tool: find contributors in a git repo that...
    ...have the same names but the different emails...

--- src/demo.rs
+++ src/demo.rs
@@ -1,3 +1,30 @@
-fn main() {
+use git2::{Commit, Repository, Revwalk};
+use std::collections::HashMap;
 
+fn main() {
+    let mut map = HashMap::new();
+
+    let repo_path = "/home/bczhc/github/rust";
+
+    let repository = Repository::open(repo_path).unwrap();
+    let mut walker = repository.revwalk().unwrap();
+
+    walker.push_head().unwrap();
+    for oid in walker {
+        let oid = oid.unwrap();
+        let commit = repository.find_commit(oid).unwrap();
+        let author = commit.author();
+        map.insert(
+            String::from(author.email().unwrap()),
+            String::from(author.name().unwrap()),
+        );
+    }
+
+    for (email, name) in &map {
+        for (email2, name2) in &map {
+            if name == name2 && email != email2 {
+                println!("{:?}", (name, email, email2));
+            }
+        }
+    }
 }
