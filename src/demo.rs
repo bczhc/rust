24ae14747bd29d79eb6713a896f431ddbf90bca7
bczhc <bczhc0@126.com>
2022-01-22 17:36:21 +0800

    hyper

--- src/demo.rs
+++ src/demo.rs
@@ -1,3 +1,14 @@
-fn main() {
+use http::Request;
+use hyper::client::HttpConnector;
+use hyper::Client;
+use hyper_tls::HttpsConnector;
 
+#[tokio::main]
+async fn main() {
+    let client = Client::builder().build::<_, hyper::Body>(HttpsConnector::new());
+    let response = client
+        .get("https://www.google.com".parse().unwrap())
+        .await
+        .unwrap();
+    println!("{:?}", response);
 }
