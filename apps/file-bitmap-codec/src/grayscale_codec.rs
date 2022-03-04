753f11fc34e63c95159d267acbfe79d1481455d2
bczhc <bczhc0@126.com>
2022-03-04 14:45:46 +0800

    file-bmp-codec::grayscale_codec: guarantee file size and avoid extra file ending zeros

--- apps/file-bitmap-codec/src/grayscale_codec.rs
+++ apps/file-bitmap-codec/src/grayscale_codec.rs
@@ -1,5 +1,6 @@
 use crate::Result;
 use bczhc_lib::io::OpenOrCreate;
+use byteorder::{ByteOrder, LittleEndian};
 use image::io::Reader;
 use image::GrayImage;
 use image::ImageFormat;
@@ -8,13 +9,22 @@ use std::io::{BufReader, BufWriter, Read, Write};
 
 pub fn encode(src: &str, dest: &str) -> Result<()> {
     let file = File::open(src)?;
-    let size = file.metadata()?.len();
+    let file_size = file.metadata()?.len();
+    let size = file_size + 8 /* packed 8-byte file-size integer */ ;
+
     let width = f64::sqrt(size as f64) as u64 + 1;
     let height = width;
     assert!(width * height >= size);
 
+    let mut packed_file_size = [0_u8; 8];
+    LittleEndian::write_u64(&mut packed_file_size, file_size);
+
     let mut image = GrayImage::new(width as u32, height as u32);
     let mut pixels_mut = image.pixels_mut();
+
+    for b in packed_file_size {
+        pixels_mut.next().unwrap().0[0] = b;
+    }
 
     let reader = BufReader::new(file);
     for b in reader.bytes() {
@@ -32,12 +42,19 @@ pub fn decode(src: &str, dest: &str) -> Result<()> {
     reader.no_limits();
     reader.set_format(ImageFormat::Bmp);
     let image = reader.decode()?.into_luma8();
+    let mut pixels = image.pixels();
 
-    let mut writer = BufWriter::new(File::open_or_create(dest)?);
-    for x in image.pixels() {
-        writer.write_all(&x.0)?;
+    let mut packed_file_size = [0_u8; 8];
+    for b in &mut packed_file_size {
+        *b = pixels.next().unwrap().0[0];
     }
 
+    let file_size = LittleEndian::read_u64(&packed_file_size);
+
+    let mut writer = BufWriter::new(File::open_or_create(dest)?);
+    for _ in 0..file_size {
+        writer.write_all(&pixels.next().unwrap().0)?;
+    }
     println!("Done");
     Ok(())
 }
