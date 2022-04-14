b71352083a8f67ee3fdc8afa57ff0f83fe05f915
bczhc <bczhc0@126.com>
2022-04-14 21:46:53 +0800

    app - forward: refactor variable names

--- apps/forward/src/main.rs
+++ apps/forward/src/main.rs
@@ -12,17 +12,17 @@ static ARGUMENTS: Lazy<RwLock<Arguments>> = Lazy::new(|| RwLock::new(Arguments::
 
 fn main() -> Result<()> {
     let matches = Command::new("forward")
-        .arg(Arg::new("address").required(true))
-        .arg(Arg::new("forward-port").required(true))
+        .arg(Arg::new("dest-addr").required(true))
+        .arg(Arg::new("listen-port").required(true))
         .get_matches();
 
-    let address = matches.value_of("address").unwrap();
-    let forward_port: u16 = matches.value_of("forward-port").unwrap().parse()?;
+    let dest_addr = matches.value_of("dest-addr").unwrap();
+    let listen_port: u16 = matches.value_of("listen-port").unwrap().parse()?;
 
-    let to_addr = address.to_socket_addrs()?.next().unwrap();
-    rw_write!(ARGUMENTS).to_addr = Some(to_addr);
+    let dest_addr = dest_addr.to_socket_addrs()?.next().unwrap();
+    rw_write!(ARGUMENTS).dest_addr = Some(dest_addr);
 
-    let listener = TcpListener::bind(SocketAddrV4::new("0.0.0.0".parse().unwrap(), forward_port))?;
+    let listener = TcpListener::bind(SocketAddrV4::new("0.0.0.0".parse().unwrap(), listen_port))?;
 
     loop {
         let accept = listener.accept()?;
@@ -36,7 +36,7 @@ fn main() -> Result<()> {
 }
 
 fn handle_connection(access_stream: TcpStream) -> Result<()> {
-    let to_addr = rw_read!(ARGUMENTS).to_addr.unwrap();
+    let to_addr = rw_read!(ARGUMENTS).dest_addr.unwrap();
     let local_stream = TcpStream::connect(to_addr)?;
 
     bczhc_lib::io::poll::poll_between_two_streams(&access_stream, &local_stream)?;
@@ -45,5 +45,5 @@ fn handle_connection(access_stream: TcpStream) -> Result<()> {
 
 #[derive(Default)]
 struct Arguments {
-    to_addr: Option<SocketAddr>,
+    dest_addr: Option<SocketAddr>,
 }
