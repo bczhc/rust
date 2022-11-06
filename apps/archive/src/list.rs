50abb916bf2ff489ea8d3128f9080100988d73b0
bczhc <bczhc0@126.com>
2022-11-06 18:44:49 +0800

    archive: list in table format and with more detailed info

--- apps/archive/src/list.rs
+++ apps/archive/src/list.rs
@@ -1,6 +1,7 @@
 use std::ffi::OsStr;
 
 use clap::ArgMatches;
+use prettytable::{format, row, Table};
 
 use crate::errors::Result;
 use crate::info::print_info;
@@ -15,12 +16,21 @@ pub fn main(matches: &ArgMatches) -> Result<()> {
     print_info(header)?;
     println!();
 
+    let mut original_size_sum = 0_u64;
+    let mut stored_size_sum = 0_u64;
+    let mut record_count = 0_u64;
+
+    let mut table = Table::new();
+    table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
+
+    table.set_titles(row!["Type", "Original", "Stored", "Ratio", "Path"]);
+
     let entries = archive.entries();
     for entry in entries {
         let entry = entry?;
         let path_bytes = &entry.path[..];
         let path = OsStr::from_bytes(path_bytes);
-        println!(
+        let path_string = format!(
             "./{}{}",
             path.to_string(),
             if !path.is_empty() && entry.file_type == FileType::Directory {
@@ -29,7 +39,42 @@ pub fn main(matches: &ArgMatches) -> Result<()> {
                 ""
             }
         );
+
+        if entry.file_type == FileType::Regular {
+            table.add_row(row![
+                format!("{:?}", entry.file_type),
+                entry.original_size,
+                entry.stored_size,
+                format!(
+                    "{:.3}",
+                    entry.original_size as f64 / entry.stored_size as f64
+                ),
+                path_string
+            ]);
+
+            stored_size_sum += entry.stored_size;
+            original_size_sum += entry.original_size;
+        } else {
+            table.add_row(row![
+                format!("{:?}", entry.file_type),
+                entry.original_size,
+                "-",
+                "-",
+                path_string
+            ]);
+        }
+        record_count += 1;
     }
+
+    table.printstd();
+    println!();
+    println!("Records: {}", record_count);
+    println!("Total original size: {}", original_size_sum);
+    println!("Total stored size: {}", stored_size_sum);
+    println!(
+        "Space saving: {:.3}%",
+        (1.0 - stored_size_sum as f64 / original_size_sum as f64) * 100.0
+    );
 
     Ok(())
 }
