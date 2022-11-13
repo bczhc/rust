a0b04afb38ba78cb8cbacdca60b12ea69803956c
bczhc <bczhc0@126.com>
2022-11-13 10:46:44 +0800

    binary-wav-codec: generate `impl`s using macros

--- apps/binary-wav-codec/src/int_reader.rs
+++ apps/binary-wav-codec/src/int_reader.rs
@@ -106,10 +106,28 @@ pub mod int_type {
     pub struct U24;
     pub struct I32;
     pub struct U32;
+    pub struct U48;
+    pub struct I48;
     pub struct I64;
     pub struct U64;
     pub struct I128;
     pub struct U128;
+}
+
+macro_rules! impl_int_readable {
+    ($t:ident, $bs:literal, $rt:ident, $f:ident) => {
+        impl<E> IntReadable<E> for int_type::$t
+        where
+            E: ByteOrder,
+        {
+            const READ_SIZE: usize = $bs / 8;
+            type ResultType = $rt;
+
+            fn read(buf: &[u8]) -> Self::ResultType {
+                E::$f(buf)
+            }
+        }
+    };
 }
 
 impl<E> IntReadable<E> for int_type::I8
@@ -132,126 +150,17 @@ where
     type ResultType = u8;
 
     fn read(buf: &[u8]) -> Self::ResultType {
-        buf[0]
+        buf[0] as u8
     }
 }
 
-impl<E> IntReadable<E> for int_type::I16
-where
-    E: ByteOrder,
-{
-    const READ_SIZE: usize = 2;
-    type ResultType = i16;
-
-    fn read(buf: &[u8]) -> Self::ResultType {
-        E::read_i16(buf)
-    }
-}
-
-impl<E> IntReadable<E> for int_type::U16
-where
-    E: ByteOrder,
-{
-    const READ_SIZE: usize = 2;
-    type ResultType = u16;
-
-    fn read(buf: &[u8]) -> Self::ResultType {
-        E::read_u16(buf)
-    }
-}
-
-impl<E> IntReadable<E> for int_type::I24
-where
-    E: ByteOrder,
-{
-    const READ_SIZE: usize = 3;
-    type ResultType = i32;
-
-    fn read(buf: &[u8]) -> Self::ResultType {
-        E::read_i24(buf)
-    }
-}
-
-impl<E> IntReadable<E> for int_type::U24
-where
-    E: ByteOrder,
-{
-    const READ_SIZE: usize = 3;
-    type ResultType = u32;
-
-    fn read(buf: &[u8]) -> Self::ResultType {
-        E::read_u24(buf)
-    }
-}
-
-impl<E> IntReadable<E> for int_type::I32
-where
-    E: ByteOrder,
-{
-    const READ_SIZE: usize = 4;
-    type ResultType = i32;
-
-    fn read(buf: &[u8]) -> Self::ResultType {
-        E::read_i32(buf)
-    }
-}
-
-impl<E> IntReadable<E> for int_type::U32
-where
-    E: ByteOrder,
-{
-    const READ_SIZE: usize = 4;
-    type ResultType = u32;
-
-    fn read(buf: &[u8]) -> Self::ResultType {
-        E::read_u32(buf)
-    }
-}
-
-impl<E> IntReadable<E> for int_type::I64
-where
-    E: ByteOrder,
-{
-    const READ_SIZE: usize = 8;
-    type ResultType = i64;
-
-    fn read(buf: &[u8]) -> Self::ResultType {
-        E::read_i64(buf)
-    }
-}
-
-impl<E> IntReadable<E> for int_type::U64
-where
-    E: ByteOrder,
-{
-    const READ_SIZE: usize = 8;
-    type ResultType = u64;
-
-    fn read(buf: &[u8]) -> Self::ResultType {
-        E::read_u64(buf)
-    }
-}
-
-impl<E> IntReadable<E> for int_type::I128
-where
-    E: ByteOrder,
-{
-    const READ_SIZE: usize = 16;
-    type ResultType = i128;
-
-    fn read(buf: &[u8]) -> Self::ResultType {
-        E::read_i128(buf)
-    }
-}
-
-impl<E> IntReadable<E> for int_type::U128
-where
-    E: ByteOrder,
-{
-    const READ_SIZE: usize = 16;
-    type ResultType = u128;
-
-    fn read(buf: &[u8]) -> Self::ResultType {
-        E::read_u128(buf)
-    }
-}
+impl_int_readable!(U24, 24, u32, read_u24);
+impl_int_readable!(I24, 24, i32, read_i24);
+impl_int_readable!(U32, 32, u32, read_u32);
+impl_int_readable!(I32, 32, i32, read_i32);
+impl_int_readable!(U48, 48, i64, read_i64);
+impl_int_readable!(I48, 48, i64, read_i64);
+impl_int_readable!(U64, 64, u64, read_u64);
+impl_int_readable!(I64, 64, i64, read_i64);
+impl_int_readable!(U128, 128, i128, read_i128);
+impl_int_readable!(I128, 128, i128, read_i128);
