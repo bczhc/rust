9f6747dffe20023afb763d416beb510e73776899
bczhc <bczhc0@126.com>
2022-11-20 14:40:29 +0800

    char-count: add Unicode grapheme cluster option

--- apps/char-count/src/main.rs
+++ apps/char-count/src/main.rs
@@ -1,29 +1,27 @@
-use bczhc_lib::utf8::utf8_bytes_length;
+use std::io::stdin;
 
-use std::io::{stdin, Read};
+use bczhc_lib::char::CharReader;
+use clap::{Arg, ArgAction, Command};
+use unicode_segmentation::UnicodeSegmentation;
 
 fn main() {
-    let length = char_length(&mut stdin());
-    println!("{:?}", length);
-}
+    let matches = Command::new("char-count")
+        .arg(
+            Arg::new("grapheme")
+                .short('g')
+                .long("grapheme")
+                .help("Count Unicode grapheme clusters")
+                .action(ArgAction::SetTrue),
+        )
+        .get_matches();
 
-fn char_length<T>(reader: &mut T) -> i32
-where
-    T: Read,
-{
-    let mut buf: [u8; 4] = [0, 0, 0, 0];
+    let chars = CharReader::new(stdin().lock());
+    let grapheme = matches.get_flag("grapheme");
 
-    let mut c = 0_i32;
-    loop {
-        let result = reader.read_exact(&mut buf[0..1]);
-        if result.is_err() {
-            break;
-        }
-
-        let length = utf8_bytes_length(buf[0]);
-        // skip (length - 1) bytes
-        std::io::copy(&mut reader.take(length as u64 - 1), &mut std::io::sink()).unwrap();
-        c += 1;
-    }
-    c
+    let length = if grapheme {
+        String::from_iter(chars).graphemes(true).count()
+    } else {
+        chars.count()
+    };
+    println!("{}", length);
 }
