67a0fadc9604adbe559625d02a95d3fa8c82db6a
bczhc <bczhc0@126.com>
2022-12-09 19:30:05 +0800

    tcp-crypto-tunnel: WIP

--- apps/tcp-crypto-tunnel/src/cli.rs
+++ apps/tcp-crypto-tunnel/src/cli.rs
@@ -1,16 +1,27 @@
-use clap::{Arg, Command};
+use clap::{value_parser, Arg, Command};
 
 pub fn build_cli() -> Command {
     Command::new("tcp-wrapper")
         .about("A TCP stream wrapper with cryptography")
         .subcommand(
             Command::new("server")
-                .arg(Arg::new("address").help("Target address"))
-                .arg(Arg::new("port").help("Port to listen on")),
+                .arg(Arg::new("address").help("Target address").required(true))
+                .arg(
+                    Arg::new("port")
+                        .help("Port to listen on")
+                        .value_parser(value_parser!(u16))
+                        .required(true),
+                ),
         )
         .subcommand(
             Command::new("client")
-                .arg(Arg::new("server").help("Server address"))
-                .arg(Arg::new("port").help("Local mapped port to listen on")),
+                .arg(Arg::new("server").help("Server address").required(true))
+                .arg(
+                    Arg::new("port")
+                        .help("Local mapped port to listen on")
+                        .value_parser(value_parser!(u16))
+                        .required(true),
+                ),
         )
+        .subcommand_required(true)
 }
--- apps/tcp-crypto-tunnel/src/server.rs
+++ apps/tcp-crypto-tunnel/src/server.rs
@@ -1,6 +1,31 @@
-use clap::ArgMatches;
 use crate::errors::*;
+use bczhc_lib::io::interact_two_stream;
+use bczhc_lib::mutex_lock;
+use clap::ArgMatches;
+use once_cell::sync::Lazy;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener, TcpStream};
+use std::sync::Mutex;
+use std::thread::spawn;
+
+static TARGET_ADDRESS: Lazy<Mutex<Option<String>>> = Lazy::new(|| Mutex::new(None));
 
 pub fn main(matches: &ArgMatches) -> Result<()> {
+    let address = matches.get_one::<String>("address").unwrap();
+    let port = *matches.get_one::<u16>("port").unwrap();
+
+    mutex_lock!(TARGET_ADDRESS).replace(address.clone());
+
+    let listener = TcpListener::bind(SocketAddr::new("0.0.0.0".parse().unwrap(), port))?;
+    loop {
+        let client = listener.accept()?;
+        println!("Accepted: {}", client.1);
+        let client = client.0;
+        spawn(move || handle_client(client).unwrap());
+    }
+}
+
+fn handle_client(mut stream: TcpStream) -> Result<()> {
+    let mut target_stream = TcpStream::connect(mutex_lock!(TARGET_ADDRESS).as_ref().unwrap())?;
+
     Ok(())
 }
