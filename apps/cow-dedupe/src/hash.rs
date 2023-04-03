fd2d9c3f4833dd724ea6e30f8f056160f263e187
bczhc <bczhc0@126.com>
2023-04-03 10:53:28 +0800

    cow-dedupe: dynamic hasher

--- apps/cow-dedupe/src/hash.rs
+++ apps/cow-dedupe/src/hash.rs
@@ -1,4 +1,7 @@
-use std::io::Write;
+use std::hash::Hasher;
+use std::io::{Read, Write};
+use std::mem::MaybeUninit;
+use std::{io, ptr, slice};
 
 use digest::Digest;
 use sha2::{Sha256, Sha512};
@@ -97,13 +100,119 @@ impl<D, const L: usize> Write for LengthDigestWrapper<D, L>
 where
     D: LengthDigest<L>,
 {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         self.0.update(buf);
         Ok(buf.len())
     }
 
-    fn flush(&mut self) -> std::io::Result<()> {
+    fn flush(&mut self) -> io::Result<()> {
         Ok(())
+    }
+}
+
+/// for `Hash` small vec, in bytes
+const HASH_RESULT_MAX_LENGTH: usize = 64;
+
+/// small vector on stack
+pub struct Hash {
+    container: [MaybeUninit<u8>; HASH_RESULT_MAX_LENGTH],
+    size: usize,
+}
+
+impl Hash {
+    pub fn new(data: &[u8]) -> Self {
+        let mut container = [MaybeUninit::uninit(); HASH_RESULT_MAX_LENGTH];
+        unsafe {
+            ptr::copy(data.as_ptr(), container.as_mut_ptr() as *mut u8, data.len());
+        }
+        Self {
+            container,
+            size: data.len(),
+        }
+    }
+}
+
+impl std::hash::Hash for Hash {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        unsafe { slice::from_raw_parts(self.container.as_ptr() as *const u8, self.size) }
+            .hash(state);
+    }
+}
+
+pub trait DynHash {
+    fn digest(&self, reader: &mut dyn Read, output: &mut [u8]);
+
+    fn length(&self) -> usize;
+}
+
+struct DynSha256Hasher;
+struct DynSha512Hasher;
+struct DynB3_256Hasher;
+struct DynB3_512Hasher;
+struct DynB3_1024Hasher;
+
+fn digest_reader<const L: usize>(
+    mut reader: impl Read,
+    digest: impl LengthDigest<L>,
+    output: &mut [u8],
+) {
+    let mut wrapper = LengthDigestWrapper(digest);
+    io::copy(&mut reader, &mut wrapper).unwrap();
+    let output_len = output.len();
+    if output_len < L {
+        panic!("output array is too small to hold the digest output");
+    }
+    output[..L].copy_from_slice(&wrapper.0.finalize_len());
+}
+
+impl DynHash for DynSha256Hasher {
+    fn digest(&self, reader: &mut dyn Read, output: &mut [u8]) {
+        digest_reader(reader, Sha256Digest::new(), output);
+    }
+
+    fn length(&self) -> usize {
+        256 / 8
+    }
+}
+
+impl DynHash for DynSha512Hasher {
+    fn digest(&self, reader: &mut dyn Read, output: &mut [u8]) {
+        digest_reader(reader, Sha512Digest::new(), output);
+    }
+
+    fn length(&self) -> usize {
+        512 / 8
+    }
+}
+
+impl DynHash for DynB3_256Hasher {
+    fn digest(&self, reader: &mut dyn Read, output: &mut [u8]) {
+        digest_reader(reader, B3_256Digest::new(), output);
+    }
+
+    fn length(&self) -> usize {
+        256 / 8
+    }
+}
+
+impl DynHash for DynB3_512Hasher {
+    fn digest(&self, reader: &mut dyn Read, output: &mut [u8]) {
+        digest_reader(reader, B3_512Digest::new(), output);
+    }
+
+    fn length(&self) -> usize {
+        512 / 8
+    }
+}
+
+impl DynHash for DynB3_1024Hasher {
+    fn digest(&self, reader: &mut dyn Read, output: &mut [u8]) {
+        const L: usize = 1024 / 8;
+        digest_reader(reader, B3Digest::<L>::new(), output);
+    }
+
+    fn length(&self) -> usize {
+        1024 / 8
     }
 }
 
@@ -112,7 +221,10 @@ mod test {
     use std::io;
     use std::io::Cursor;
 
-    use crate::hash::{B3Digest, LengthDigest, LengthDigestWrapper, Sha256Digest, Sha512Digest};
+    use crate::hash::{
+        B3Digest, DynB3_1024Hasher, DynB3_256Hasher, DynB3_512Hasher, DynHash, DynSha256Hasher,
+        DynSha512Hasher, LengthDigest, LengthDigestWrapper, Sha256Digest, Sha512Digest,
+    };
 
     fn digest<const L: usize>(digest: impl LengthDigest<L>, data: &[u8]) -> [u8; L] {
         let mut wrapper = LengthDigestWrapper(digest);
@@ -129,5 +241,24 @@ mod test {
         println!("{:?}", digest(B3Digest::<1>::new(), &data));
         println!("{:?}", digest(B3Digest::<32>::new(), &data));
         println!("{:?}", digest(B3Digest::<64>::new(), &data));
+    }
+
+    #[test]
+    fn test2() {
+        let data = b"hello, world";
+        let hasher: [&dyn DynHash; 5] = [
+            &DynB3_512Hasher,
+            &DynB3_256Hasher,
+            &DynSha512Hasher,
+            &DynSha256Hasher,
+            &DynB3_1024Hasher,
+        ];
+        let max_length = hasher.iter().map(|x| x.length()).max().unwrap();
+        for h in hasher {
+            let mut reader = Cursor::new(data);
+            let mut output = vec![0_u8; max_length];
+            h.digest(&mut reader, &mut output);
+            println!("{:?}", &output[..h.length()]);
+        }
     }
 }
