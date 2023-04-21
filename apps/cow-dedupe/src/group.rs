00dc334d7e860dd02dedc5c83d76bbdd0fb541f1
bczhc <bczhc0@126.com>
2023-04-21 19:45:43 +0800

    Revert "cow-dedupe: dynamic hasher"
    This reverts commit fd2d9c3f4833dd724ea6e30f8f056160f263e187.
    
    Don't need it. I still decide to use static dispatch.

--- apps/cow-dedupe/src/group.rs
+++ apps/cow-dedupe/src/group.rs
@@ -1,36 +1,5 @@
 use crate::cli::GroupArgs;
-use crate::{group_by_middle_content, group_by_size};
-use std::fs::FileType;
-use std::io;
-use std::path::PathBuf;
 
 pub fn main(args: &GroupArgs) -> anyhow::Result<()> {
-    let paths = &args.common.path;
-    eprintln!("Collecting files...");
-    let mut files = collect_file(paths)?;
-    eprintln!("File entries: {}", files.len());
-    eprintln!("Grouping by size...");
-    group_by_size(&mut files);
-    eprintln!("File entries: {}", files.len());
-    eprintln!("Grouping by middle content...");
-    group_by_middle_content(&mut files)?;
-    eprintln!("File entries: {}", files.len());
-
     Ok(())
-}
-
-/// Returns (path, file size)
-fn collect_file(paths: &Vec<String>) -> io::Result<Vec<(PathBuf, u64)>> {
-    let mut files_vec = Vec::new();
-    for path in paths {
-        let files = walkdir::WalkDir::new(path);
-        for entry in files {
-            let entry = entry?;
-            if !entry.file_type().is_file() {
-                continue;
-            }
-            files_vec.push((entry.path().into(), entry.metadata()?.len()));
-        }
-    }
-    Ok(files_vec)
 }
--- apps/cow-dedupe/src/lib.rs
+++ apps/cow-dedupe/src/lib.rs
@@ -1,17 +1,5 @@
-use std::io;
-use std::path::Path;
-
 pub mod cli;
 pub mod dedupe;
 pub mod errors;
 pub mod group;
 pub mod hash;
-
-pub fn group_by_size<P: AsRef<Path>>(path: &mut Vec<(P, u64)>) {
-    path.sort_by_key(|x| x.1);
-    path.dedup_by_key(|x| x.1);
-}
-
-pub fn group_by_middle_content<P: AsRef<Path>>(path: &mut Vec<(P, u64)>) -> io::Result<()> {
-    Ok(())
-}
