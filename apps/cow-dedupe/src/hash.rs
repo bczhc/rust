69ccc6d7b91f1d1ea8c86157451427d4e8d90d24
bczhc <bczhc0@126.com>
2023-04-21 20:47:40 +0800

    cow-dedupe: implement `FixedOutput` for blake3-256, -512 and -1024

--- apps/cow-dedupe/src/hash.rs
+++ apps/cow-dedupe/src/hash.rs
@@ -1,6 +1,8 @@
-use digest::Update;
 use std::io;
 use std::io::Write;
+
+use digest::consts::{U128, U64};
+use digest::{ExtendableOutput, Output, Update};
 
 pub struct HashWriter<H>(pub H)
 where
@@ -19,3 +21,39 @@ where
         Ok(())
     }
 }
+
+pub type B3_256 = blake3::Hasher;
+
+macro_rules! impl_b3_xof {
+    ($name:ident, $size:ty) => {
+        #[derive(Default)]
+        pub struct $name {
+            inner: blake3::Hasher,
+        }
+
+        impl $name {
+            pub fn new() -> Self {
+                Self::default()
+            }
+        }
+
+        impl Update for $name {
+            fn update(&mut self, data: &[u8]) {
+                Update::update(&mut self.inner, data);
+            }
+        }
+
+        impl digest::OutputSizeUser for $name {
+            type OutputSize = $size;
+        }
+
+        impl digest::FixedOutput for $name {
+            fn finalize_into(self, out: &mut Output<Self>) {
+                self.inner.finalize_xof_into(out);
+            }
+        }
+    };
+}
+
+impl_b3_xof!(B3_512, U64);
+impl_b3_xof!(B3_1024, U128);
