eae707f49d3b4666d17cedd7e3aaec778e40d3c7
bczhc <bczhc0@126.com>
2023-04-22 09:42:16 +0800

    cow-dedupe: group: group files instead of dedup

--- apps/cow-dedupe/src/group.rs
+++ apps/cow-dedupe/src/group.rs
@@ -1,49 +1,65 @@
-use crate::cli::{GroupArgs, HashFn};
-use crate::hash::{FixedDigest, B3_1024, B3_256, B3_512};
-use crate::{group_by_content, group_by_fragments, group_by_size};
+use std::io;
+use std::path::PathBuf;
+
 use digest::generic_array::GenericArray;
 use digest::typenum::Unsigned;
 use sha2::{Sha256, Sha512};
-use std::io;
-use std::path::PathBuf;
+
+use crate::cli::{GroupArgs, HashFn};
+use crate::hash::{FixedDigest, B3_1024, B3_256, B3_512};
+use crate::{group_by_content, group_by_size};
 
 pub fn main(args: &GroupArgs) -> anyhow::Result<()> {
     let paths = &args.common.path;
     eprintln!("Collecting files...");
-    let mut files = collect_file(paths)?;
-    eprintln!("File entries: {}", files.len());
+    let mut entries = collect_file(paths)?;
+    eprintln!("File entries: {}", entries.len());
     eprintln!("Grouping by size...");
-    group_by_size(&mut files);
-    eprintln!("File entries: {}", files.len());
+    let mut groups = group_by_size(&mut entries);
+    eprintln!("File entries: {}", entries.len());
 
     match args.common.hash_fn {
-        HashFn::B3_256 => generic_group_files_using_hash::<B3_256>(&mut files),
-        HashFn::B3_512 => generic_group_files_using_hash::<B3_512>(&mut files),
-        HashFn::B3_1024 => generic_group_files_using_hash::<B3_1024>(&mut files),
-        HashFn::Sha256 => generic_group_files_using_hash::<Sha256>(&mut files),
-        HashFn::Sha512 => generic_group_files_using_hash::<Sha512>(&mut files),
+        HashFn::B3_256 => generic_group_files_by_hash::<B3_256>(&mut groups),
+        HashFn::B3_512 => generic_group_files_by_hash::<B3_512>(&mut groups),
+        HashFn::B3_1024 => generic_group_files_by_hash::<B3_1024>(&mut groups),
+        HashFn::Sha256 => generic_group_files_by_hash::<Sha256>(&mut groups),
+        HashFn::Sha512 => generic_group_files_by_hash::<Sha512>(&mut groups),
     }?;
-
-    // print these entries out
 
     Ok(())
 }
 
-fn generic_group_files_using_hash<H: FixedDigest>(files: &mut Vec<FileEntry>) -> anyhow::Result<()>
+fn generic_group_files_by_hash<H: FixedDigest>(
+    files: &mut Vec<Vec<FileEntry>>,
+) -> anyhow::Result<()>
 where
     [(); H::OutputSize::USIZE]:,
     [u8; H::OutputSize::USIZE]: From<GenericArray<u8, H::OutputSize>>,
 {
     eprintln!("Grouping by file fragments");
-    group_by_fragments(files)?;
+    // group_by_fragments(files)?;
     eprintln!("File entries: {}", files.len());
     eprintln!("Grouping by file content...");
-    group_by_content::<H>(files)?;
+    let groups = group_by_content::<H>(files)?;
     eprintln!("File entries: {}", files.len());
+
+    // print out
+    for x in groups {
+        println!(
+            "{}, {}",
+            hex::encode(x.0),
+            bytesize::to_string(x.1[0].size, true)
+        );
+        for x in x.1 {
+            println!("{}", x.path.display());
+        }
+        println!()
+    }
 
     Ok(())
 }
 
+#[derive(Clone, Debug)]
 pub struct FileEntry {
     pub path: PathBuf,
     pub size: u64,
--- apps/cow-dedupe/src/lib.rs
+++ apps/cow-dedupe/src/lib.rs
@@ -1,5 +1,8 @@
+#![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
+#![feature(slice_group_by)]
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 
@@ -16,32 +19,45 @@ pub mod errors;
 pub mod group;
 pub mod hash;
 
-pub fn group_by_size(entries: &mut Vec<FileEntry>) {
+pub fn group_by_size(entries: &mut Vec<FileEntry>) -> Vec<Vec<FileEntry>> {
     entries.sort_by_key(|x| x.size);
-    entries.dedup_by_key(|x| x.size);
+    entries
+        .group_by(|a, b| a.size == b.size)
+        .map(Vec::from)
+        .collect::<Vec<_>>()
 }
 
 /// 50 bytes after file start
 /// 50 bytes before file end
 /// middle 100 bytes
-pub fn group_by_fragments(entries: &mut Vec<FileEntry>) -> io::Result<()> {
-    Ok(())
+pub fn group_by_fragments(entries: &mut Vec<Vec<FileEntry>>) -> io::Result<()> {
+    todo!()
 }
 
-pub fn group_by_content<H: FixedDigest>(entries: &mut Vec<FileEntry>) -> io::Result<()>
+pub fn group_by_content<H: FixedDigest>(
+    entries: &mut Vec<Vec<FileEntry>>,
+) -> io::Result<HashMap<[u8; H::OutputSize::USIZE], Vec<FileEntry>>>
 where
     [(); H::OutputSize::USIZE]:,
     [u8; H::OutputSize::USIZE]: From<GenericArray<u8, H::OutputSize>>,
 {
-    let mut vec: Vec<(&FileEntry, [u8; H::OutputSize::USIZE])> = Vec::new();
-    for x in entries {
-        let mut hasher = HashWriter(H::new());
-        let mut file = File::open(&x.path)?;
-        io::copy(&mut file, &mut hasher).unwrap();
-        let digest: [u8; H::OutputSize::USIZE] = hasher.0.finalize_fixed().into();
-        vec.push((x, digest));
+    let mut groups = HashMap::new();
+
+    for g in entries {
+        let mut vec: Vec<(FileEntry, [u8; H::OutputSize::USIZE])> = Vec::new();
+        for x in g.iter() {
+            let mut hasher = HashWriter(H::new());
+            let mut file = File::open(&x.path)?;
+            io::copy(&mut file, &mut hasher)?;
+            let digest: [u8; H::OutputSize::USIZE] = hasher.0.finalize_fixed().into();
+            vec.push((x.clone(), digest));
+        }
+        vec.sort_by_key(|x| x.1);
+        for x in vec.group_by(|a, b| a.1 == b.1) {
+            let hash = x[0].1;
+            let group = x.iter().map(|x| x.0.clone()).collect::<Vec<_>>();
+            groups.insert(hash, group);
+        }
     }
-    vec.sort_by_key(|x| x.1);
-    vec.dedup_by_key(|x| x.1);
-    Ok(())
+    Ok(groups)
 }
