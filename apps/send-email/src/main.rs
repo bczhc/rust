d7ae213e123b14b8aa5757173a8b81d835b09ca4
Zhai Can <bczhc0@126.com>
2023-08-09 18:22:06 +0800

    send-email: use `anyhow` and handle all `unwrap()` if in need

--- apps/send-email/src/main.rs
+++ apps/send-email/src/main.rs
@@ -1,16 +1,21 @@
+#![feature(try_blocks)]
+
 use std::fs::File;
 use std::io::{stdin, Read};
 use std::process::abort;
 use std::thread::{sleep, spawn};
 use std::time::Duration;
 
+use anyhow::anyhow;
 use lettre::message::header::ContentType;
 use lettre::transport::smtp::authentication::Credentials;
+use lettre::transport::smtp::response::Response;
 use lettre::{SmtpTransport, Transport};
+
 use send_email::cli::build_cli;
 use send_email::{Account, Config, Message};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> anyhow::Result<()> {
     let matches = build_cli().get_matches();
 
     let config_file = matches.get_one::<String>("config").unwrap();
@@ -30,18 +35,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     let mut config = String::new();
-    File::open(config_file)
-        .unwrap()
-        .read_to_string(&mut config)
-        .unwrap();
-    let config: toml::Value = toml::from_str(&config).unwrap();
+    File::open(config_file)?.read_to_string(&mut config)?;
+    let config: toml::Value = toml::from_str(&config)?;
 
     let email_cfg = &config["email"];
-    let config = Config {
-        smtp: email_cfg["smtp"].as_str().unwrap().into(),
-        from: email_cfg["from"].as_str().unwrap().into(),
-        username: email_cfg["username"].as_str().unwrap().into(),
-        password: email_cfg["password"].as_str().unwrap().into(),
+    let config: Option<Config> = try {
+        Config {
+            smtp: email_cfg["smtp"].as_str()?.into(),
+            from: email_cfg["from"].as_str()?.into(),
+            username: email_cfg["username"].as_str()?.into(),
+            password: email_cfg["password"].as_str()?.into(),
+        }
+    };
+    let Some(config) = config else {
+        return Err(anyhow!("Wrong TOML format"))
     };
 
     let credentials = Credentials::new(config.username.clone(), config.password.clone());
@@ -56,7 +63,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         body,
     };
 
-    let send_thread = spawn(move || send_email(message, account));
     if let Some(&timeout) = timeout {
         spawn(move || {
             sleep(Duration::from_millis(timeout as u64));
@@ -65,39 +71,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             abort();
         });
     }
-
-    let result = send_thread.join().unwrap();
-    match result {
-        Ok(_) => {
-            println!("Done");
-            Ok(())
-        }
-        Err(e) => Err(e.into()),
-    }
+    let _response = send_email(message, account)?;
+    println!("Done");
+    Ok(())
 }
 
-fn send_email(
-    message: Message,
-    account: Account,
-) -> Result<lettre::transport::smtp::response::Response, lettre::transport::smtp::Error> {
+fn send_email(message: Message, account: Account) -> anyhow::Result<Response> {
     let mut email = lettre::Message::builder()
-        .from(message.from.parse().unwrap())
-        .to(message.to.parse().unwrap());
+        .from(message.from.parse()?)
+        .to(message.to.parse()?);
 
     if let Some(ref s) = message.subject {
         email = email.subject(s);
     }
-    let email = email
-        .header(ContentType::TEXT_PLAIN)
-        .body(message.body)
-        .unwrap();
+    let email = email.header(ContentType::TEXT_PLAIN).body(message.body)?;
 
     let creds = account.credentials.clone();
 
-    let mailer = SmtpTransport::relay(&account.smtp)
-        .unwrap()
+    let mailer = SmtpTransport::relay(&account.smtp)?
         .credentials(creds)
         .build();
 
-    mailer.send(&email)
+    Ok(mailer.send(&email)?)
 }
